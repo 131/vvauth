eae616779ddc619eac13b7c80fc37785747e76b2
Francois Leurent <131.code@leurent.email>
2025-02-27 15:12:54 +0100

    Initial draft

---
+++ README.md
@@ -0,0 +1,6 @@
+Vault Creds manager
+
+This projects helps you log yourself in a HCL vault and retrieve VAULT_TOKEN through different auth methods
+* jwt login
+* ssh (with agent) login
+
---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+class vcreds {
+
+
+
+}
+
+
+if(module.parent === null) //ensure module is called directly, i.e. not required
+  require('cnyks/lib/bundle')(vcreds); //start runner
+
+
+module.exports = vcreds;
